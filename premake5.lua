ad350734c69bcd259c690f9eebe6f31d8403ebac
die-Anna <anna.stefanie.matt@gmail.com>
2023-02-18 14:16:58 +0100

    added premake (working on rider)

---
+++ premake5.lua
@@ -0,0 +1,103 @@
+workspace "Chestnut"
+    architecture "x64"
+
+    configurations {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Chestnut"
+    location "Chestnut"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs {
+        --"$(SolutionDir)Chestnut/vendor/spdlog/include"
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "10.0"
+
+        defines {
+            "CN_PLATFORM_WINDOWS",
+            "CN_BUILD_DLL"
+        }
+
+        postbuildcommands {
+            ("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Sandbox/\"")
+        }
+    
+    filter "configurations:Debug"
+        defines "CN_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "CN_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "CN_DIST"
+        optimize "On"
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs {
+        "Chestnut/vendor/spdlog/include",
+        "Chestnut/src"
+    }
+
+    links {
+        "Chestnut"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "10.0"
+
+        defines {
+            "CN_PLATFORM_WINDOWS"        }
+
+        postbuildcommands {
+            --("{COPY} %{cfg.buildtarget.relpath} .. /bin/" .. outputdir .. "/Sandbox")
+            --("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Sandbox/\"")
+        } 
+    
+    filter "configurations:Debug"
+        defines "CN_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "CN_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "CN_DIST"
+        optimize "On"
+   
+
